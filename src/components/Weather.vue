6200b5f8ebbdb126386cad542861ab17624d23d0
dineshRJ <dinesh@moonraft.com>
2022-02-02 00:59:49 +0530

    initial commit

---
+++ src/components/Weather.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>Hello</p>
+</template>
+
+<script>
+export default {
+  name: "Weather",
+  data() {
+    return {
+      api_key: "ac7a5e73b81ab05458b94ed86919066d",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
