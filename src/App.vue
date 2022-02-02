00309396ea5ecca7af415b54783fd85096fbd513
dineshRJ <dinesh@moonraft.com>
2022-02-02 20:37:53 +0530

    background logic change in app component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <div id="app">
-    <Weather />
+  <div id="app" :class="temperature > 16 ? 'warm' : 'cold'">
+    <main>
+      <Weather @updateWeather="updateWeather($event)" />
+    </main>
   </div>
 </template>
 
@@ -9,11 +11,47 @@ import Weather from "./components/Weather.vue";
 
 export default {
   name: "App",
+  data() {
+    return {
+      temperature: 0,
+    };
+  },
   components: {
     Weather,
+  },
+  methods: {
+    updateWeather(temperature) {
+      this.temperature = temperature;
+    },
   },
 };
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+body {
+  font-family: "Raleway", sans-serif;
+  margin: 0;
+  padding: 0;
+}
+#app {
+  background-image: url("./assets/cold-bg.jpeg");
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+#app.warm {
+  background-image: url("./assets/warm-bg.jpeg");
+}
+main {
+  min-height: 100vh;
+  padding: 24px;
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.25),
+    rgba(0, 0, 0, 0.75)
+  );
+}
 </style>
